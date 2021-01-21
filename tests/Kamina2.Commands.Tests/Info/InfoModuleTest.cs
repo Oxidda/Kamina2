0e471f8b1541ae8bd9fedfa5ec7b8397e74ab00d
Dennis Tang <dennis.sht@gmail.com>
2021-01-21 19:51:55 +0100

    Introduced options to retrieve custom attributes from a method.

--- tests/Kamina2.Commands.Tests/Info/InfoModuleTest.cs
+++ tests/Kamina2.Commands.Tests/Info/InfoModuleTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Threading.Tasks;
 using Discord.Commands;
 using Kamina2.Commands.Info;
@@ -76,6 +77,67 @@ namespace Kamina2.Commands.Tests.Info
             // Assert
             service.DidNotReceiveWithAnyArgs().GetCurrent(); // Do not expect the service being called
             var temp = service.DidNotReceive().StartTime; // to check properties, temp variables have to be introduced or this results in a compilation error
+        }
+
+        [Test]
+        public void UserInfoAsyncWithoutArguments_Always_ReturnsExpectedAttributes()
+        {
+            // Call
+            var commandAttribute = GetCustomAttribute<InfoModule, CommandAttribute>(nameof(InfoModule.UserInfoAsync));
+            var summaryAttribute = GetCustomAttribute<InfoModule, SummaryAttribute>(nameof(InfoModule.UserInfoAsync));
+
+            // Assert
+            Assert.That(commandAttribute, Is.Not.Null);
+            Assert.That(commandAttribute.Text, Is.EqualTo("info"));
+
+            const string expectedSummaryText = "Returns info about the current user, or the user parameter, if one passed.";
+            Assert.That(summaryAttribute, Is.Not.Null);
+            Assert.That(summaryAttribute.Text, Is.EqualTo(expectedSummaryText));
+        }
+
+        [Test]
+        public void UserInfoAsyncWithMessageArgument_Always_ReturnsExpectedAttributes()
+        {
+            // Setup
+            var argumentTypes = new[]
+            {
+                typeof(string)
+            };
+
+            // Call
+            var commandAttribute = GetCustomAttribute<InfoModule, CommandAttribute>(nameof(InfoModule.UserInfoAsync), argumentTypes);
+            var aliasAttribute = GetCustomAttribute<InfoModule, AliasAttribute>(nameof(InfoModule.UserInfoAsync), argumentTypes);
+
+            // Assert
+            Assert.That(commandAttribute, Is.Not.Null);
+            Assert.That(commandAttribute.Text, Is.EqualTo("say"));
+
+            Assert.That(aliasAttribute, Is.Not.Null);
+
+            // Asserts a string collection:
+            var aliasValues = aliasAttribute.Aliases;
+            Assert.That(aliasValues, Has.Length.EqualTo(1));
+            Assert.That(aliasValues[0], Is.EqualTo("respond"));
+
+            // Or alternatively:
+            CollectionAssert.AreEqual(new[]
+            {
+                "respond"
+            }, aliasValues);
+        }
+
+        private static TAttribute GetCustomAttribute<TObject, TAttribute>(string methodName)
+            where TObject : class
+            where TAttribute : Attribute
+        {
+            return GetCustomAttribute<TObject, TAttribute>(methodName, Type.EmptyTypes);
+        }
+
+        private static TAttribute GetCustomAttribute<TObject, TAttribute>(string methodName, Type[] argumentTypes)
+            where TObject : class
+            where TAttribute : Attribute
+        {
+            return typeof(TObject).GetMethod(methodName, argumentTypes)?.GetCustomAttribute<TAttribute>();
         }
     }
 }
